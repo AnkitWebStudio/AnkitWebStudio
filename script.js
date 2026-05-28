8154aedf62ff2ac0b689352ea6bebef1549ff9ef
AnkitWebStudio <ankitwebsolution08@gmail.com>
2026-05-28 20:23:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    document.getElementById("sidebar").classList.toggle("active");
+
+
+document.getElementById("overlay1")
+    .classList.toggle("active");
+
+
+
+}
+
+
+function backsidebar(){
+
+
+document.getElementById("sidebar").classList.remove("active");
+document.getElementById("overlay1").classList.remove("active");
+
+
+
+}
